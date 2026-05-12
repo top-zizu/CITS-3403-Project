07934d5936bc910a6908e2170d49f96065a7b97d
ChazzLord <ygochazz@gmail.com>
2026-05-12 15:17:19 +0800

    Update sidebar.js
    update sidebar friends link to point to followers instead of fake requests

--- static/sidebar.js
+++ static/sidebar.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <span>Friends</span>
         </a>
 
-        <a href="/friends?tab=requests" class="sidebar-link">
+        <a href="/friends?tab=followers" class="sidebar-link">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor"
             stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <line x1="20" y1="8" x2="20" y2="14"/>
             <line x1="17" y1="11" x2="23" y2="11"/>
           </svg>
-          <span>Friend Requests</span>
+          <span>Followers</span>
         </a>
 
         <a href="/notifications" class="sidebar-link">
